cee7a24ba6ceff2c1aff2f3afb9ef779ea754c58
Shadab Akhtar <0112cs211108@gmail.com>
2024-09-01 18:08:41 +0530

    conversion of datatype

---
+++ 03_datatypes.js
@@ -0,0 +1,31 @@
+let score = "shadab"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number (score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN (not a number)
+// true => 1; false => 0
+
+let isLoggedIn = 1
+let isLoggedIns = "shadab"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+let booleanIsLoggedIns = Boolean(isLoggedIns)
+// console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIns);
+
+// 1 => true; 0 => false
+// "" => false
+// "shadab" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof (stringNumber));
+
