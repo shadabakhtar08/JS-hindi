a1bb73f5a212711e44e08a4e4c72495791c892cf
Shadab Akhtar <0112cs211108@gmail.com>
2024-09-12 00:55:17 +0530

    datatype

---
+++ datatypes-summary.js
@@ -0,0 +1,10 @@
+// primitive :call by value
+
+//  7 types: String,Number,Boolean,null,undefined,Symbol,BigInt
+
+ 
+
+
+// Reference Type / Non Primitive: call by reference
+
+// Array,Objects,Functions
