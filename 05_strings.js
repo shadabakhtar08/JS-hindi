8c4349ccd624b6ded3700e4cfcb5c8f510e7d3b2
Shadab Akhtar <0112cs211108@gmail.com>
2024-09-15 16:15:18 +0530

    new

---
+++ 05_strings.js
@@ -0,0 +1,9 @@
+const name = "shadab"
+const repoCount = 50
+
+// console.log(name + repoCount + " value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`); 
+// string interpotation
+
+const gameName = new String('shadabhc') 
