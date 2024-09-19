a2955323bf9964ea4ef3ae65947b7127878b7561
Shadab Akhtar <0112cs211108@gmail.com>
2024-09-19 19:33:00 +0530

    objects

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,28 @@
+//  singleton : constructor se banega to ye hoga 
+// Object.create //isi ko kehte hain constructor method ke through
+
+// object literals : object ko declare karne ka tarika hai
+ //{} : cerly braces ka matlab hai object abhi khali hai to empty object hai
+
+const mySym = Symbol("key1")
+
+const JsUser = {
+    name: "Shadab",
+    "full name": "Shadab Akhtar",  //isko ab . dot use karke access nahi kar sakte
+    [mySym]: "mykey1",
+    age: 20,
+    location: "Bhopal",
+    email: "shadabakhtar@google.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
+}    
+
+console.log(JsUser.email);
+console.log(JsUser["email"]);
+console.log(JsUser["full name"]);
+console.log(JsUser[mySym]);
+
+JsUser.email = "akhtarshadab@chatgpt.com"
+Object.freeze(JsUser)
+JsUser.email = "akhtarshadab@microsoft.com"
+console.log(JsUser);
