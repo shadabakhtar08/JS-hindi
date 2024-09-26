8404896270646185e921a0294ca1719443d77a93
Shadab Akhtar <0112cs211108@gmail.com>
2024-09-26 22:33:44 +0530

    objects_part2

---
+++ 02_basics/04_objects2.js
@@ -0,0 +1,18 @@
+// const tinderUser = new Object()  //single turn object hai
+const tinderUser = {}   //non-single turn object hai
+
+tinderUser.id = "123abc"
+tinderUser.name = "shadab"
+tinderUser.isLoggedIn = false
+
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email: "shadab@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname: "shadab"
+        }
+    }
+}
