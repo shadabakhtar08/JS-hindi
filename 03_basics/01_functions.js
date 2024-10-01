66e8c871a3a4fc445b1cdd1bf55ae58ab9c6739f
Shadab Akhtar <0112cs211108@gmail.com>
2024-10-01 19:13:14 +0530

    03_basics

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -46,5 +46,33 @@ function loginUserMessage(username = "sam") {
     return `${username} just logged in`
 }
 // console.log(loginUserMessage("shadab"));
-console.log(loginUserMessage("shadab"));  //agar koi value hi nahi denge to wo undefined output dega
+// console.log(loginUserMessage("shadab"));  //agar koi value hi nahi denge to wo undefined output dega
+// "..." 3 dots ko spread operator bolte hain or isi ko rest operator bhi bolte hain
+function calculateCartPrice(val1, val2, ...num1){
+    return num1
+}
 
+// console.log(calculateCartPrice(200, 400, 500, 2000));
+
+const user = {
+    username: "Shadab",
+    price: 199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+// handleObject(user)
+handleObject({
+    username: "sam",
+    price: 399
+})
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 300, 500, 1000]));
