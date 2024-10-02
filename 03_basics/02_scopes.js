0eacdc52469d125abd3b031009cecb822d64cc66
Shadab Akhtar <0112cs211108@gmail.com>
2024-10-02 18:34:38 +0530

    02_scopes

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,18 @@
+// var c = 300
+let a = 300
+
+if (true) {
+    let a = 10
+    const b = 20
+    console.log("INNER: ", a);
+}
+
+// getComputedStylelobal scope main aap jo bhi likhenge wo scope main available rahegi magar jo bhi scope main likh hua hai wo bahr available nahi hona cahiye 
+
+// ye jo bhi if ke andar likha hua hai wo hai block scope  and uske bahar jo bhi likha hua hai wo hai global scope
+
+//{} ye scope hai,ye kisi function ke saath aata hai ya kisi if-else ke saath aata hai to isko scope bolte hain wahan 
+
+// console.log(a);
+// console.log(b);
+console.log(a);
