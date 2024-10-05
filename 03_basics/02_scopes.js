3d872c0f02c463dd670712a697b75f070e0330b1
Shadab Akhtar <0112cs211108@gmail.com>
2024-10-05 15:28:42 +0530

    02_scopes

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -4,7 +4,7 @@ let a = 300
 if (true) {
     let a = 10
     const b = 20
-    console.log("INNER: ", a);
+    // console.log("INNER: ", a);
 }
 
 // getComputedStylelobal scope main aap jo bhi likhenge wo scope main available rahegi magar jo bhi scope main likh hua hai wo bahr available nahi hona cahiye 
@@ -14,7 +14,47 @@ if (true) {
 //{} ye scope hai,ye kisi function ke saath aata hai ya kisi if-else ke saath aata hai to isko scope bolte hain wahan 
 
 // console.log(a);
+// console.log(a);
 // console.log(b);
-console.log(a);
 
+function one(){
+    const username = "shadab"
+
+    function two(){
+        const website = "youtube"
+        // console.log(username);
+    }
+    // console.log(website);    //line by line execute ho raha hai
+//jub nested function ho to child apne parent ke variable ko access kar sakte hain
+    two()
+}
+
+one()
+
+if (true) {
+    const username = "shadab"
+    if (username === "shadab") {
+        const website = " youtube"
+        // console.log(username + website);      
+    }
+    // console.log(website);  //is laine main error aayegi kyunki uska scope ke bahar access karne ki kosis kar rahe hain
+}
+// console.log(username);   //is laine main error aayega kyunki uska scope ke bahar access karne ki kosis kar rahe hain
+
+
+// +++++++++++++++++ interesting ++++++++++++++++++
+
+console.log(addone(5));
+
+function addone(num){
+    return num + 1
+}
+
+
+
+addTwo(5)  //yahan error aayega kyunki is wale way main declaration se pehle aap usko use nahi kar sakte
+
+const addTwo = function(num){
+    return num + 2
+}
 
