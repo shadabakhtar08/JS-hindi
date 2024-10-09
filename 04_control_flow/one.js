d534cc6dceb5dc4e2724493b8f3c6b0c3577fa74
Shadab Akhtar <0112cs211108@gmail.com>
2024-10-09 19:23:57 +0530

    04_control_flow

---
+++ 04_control_flow/one.js
@@ -0,0 +1,12 @@
+//  if
+
+// if (condition) {  } 
+//condition ki jagah true honi cahiye tub uske andar ka code run hoga agar false hua to andar ka code kuch nahi chalega.
+    
+const isUserloggedIn = true
+
+if (isUserloggedIn) {
+    
+}
+
+// <, >, <=, >=, ==, !=, ===, !==
