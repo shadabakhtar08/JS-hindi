1b67d50c18b92a1f63059286533c9e920dcabe98
Shadab Akhtar <0112cs211108@gmail.com>
2024-10-13 17:33:08 +0530

    04_control_flow

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,52 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }                                                                     
+
+// const month = 1
+
+// switch (month) {
+//     case 1:
+//         console.log("January");
+//         break;
+//     case 2:
+//         console.log("feb");
+//         break;
+//     case 3:
+//         console.log("march");
+//         break;             
+//     case 4:
+//         console.log("april");
+//         break;
+
+//     default:
+//         console.log("default case match");
+        
+//         break;
+// }
+
+const month = "march"
+
+switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "march":
+        console.log("march");
+        break;             
+    case "april":
+        console.log("april");
+        break;
+
+    default:
+        console.log("default case match");
+        
+        break;
+}
