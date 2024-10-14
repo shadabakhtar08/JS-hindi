70cdd3d5934c63a44672f0f175268772e6b7249e
Shadab Akhtar <0112cs211108@gmail.com>
2024-10-14 19:43:14 +0530

    05_iterations

---
+++ 05_iterations/three.js
@@ -0,0 +1,32 @@
+//  for of
+
+// ["", "", ""]
+// [{}, {}, {}]
+
+const arr = [1, 2, 3, 4, 5]
+
+for (const num of arr) {
+    // console.log(num);  
+}
+
+const greetings = "Hello world!"
+for (const greet of greetings) {
+    // console.log(`Each char is ${greet}`);  
+}
+
+// Maps
+// The Map object holds key-value pairs and remembers the original insertion order of the keys. Any value (both objects and primitive values) may be used as either a key or a value.
+
+// Map objects are collections of key-value pairs. A key in the Map may only occur once; it is unique in the Map's collection.
+
+const map = new Map()
+map.set('IN', "India")
+map.set('USA', "United States of America")
+map.set('FR', "France")
+// map.set('IN', "India")   //ek baar hi print hota hai baar baar print nahi hoga 
+
+console.log(map);
+
+for (const key of map) {
+    
+}
