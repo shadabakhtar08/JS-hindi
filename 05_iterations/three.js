93a7b6a3bc7c13234f26bae51efe2886e475bff9
Shadab Akhtar <0112cs211108@gmail.com>
2024-10-15 01:06:14 +0530

    05_iterations

--- 05_iterations/three.js
+++ 05_iterations/three.js
@@ -25,8 +25,18 @@ map.set('USA', "United States of America")
 map.set('FR', "France")
 // map.set('IN', "India")   //ek baar hi print hota hai baar baar print nahi hoga 
 
-console.log(map);
+// console.log(map);
 
-for (const key of map) {
-    
+for (const [key, value] of map) {
+    // console.log(key, ':-', value);  
 }
+
+const myObject = {
+    game1: 'NFS',
+    game2: 'Spiderman'
+}
+
+// for (const [key, value] of myObject) {
+//     console.log(key, ':-', value); 
+// }
+
